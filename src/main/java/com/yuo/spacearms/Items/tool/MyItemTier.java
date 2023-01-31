7eb12aa901ed26ca57af42c092a7029c807354b7
yuoft <2685245558@qq.com>
2023-01-31 10:09:49 +0800

    耐久伤害调整

--- src/main/java/com/yuo/spacearms/Items/tool/MyItemTier.java
+++ src/main/java/com/yuo/spacearms/Items/tool/MyItemTier.java
@@ -9,42 +9,43 @@ import net.minecraft.util.LazyValue;
 import java.util.function.Supplier;
 
 public enum MyItemTier implements IItemTier {
-    EMERALD(1332, 9.0f, 5.0f, 3, 10, () -> {
-             return Ingredient.fromItems(SAItems.emeraldIngot.get());
-    }),
-    RUBY(1214, 8.5f, 4.0f, 3, 10, () -> {
-        return Ingredient.fromItems(SAItems.rubyIngot.get());
-    }),
-    SPACE(3124, 30.0f, 40.0f, 7, 30, () -> {
-        return Ingredient.fromItems(SAItems.spaceIngot.get());
-    }),
-    SUPER_SPACE(3124, 15.0f, 12.0f, 9, 15, () -> {
-        return Ingredient.fromItems(SAItems.spaceCore.get());
-    }),
-    WOLF(233, 20.0f, 600f, 3, 0, () -> {
-        return Ingredient.fromItems(Items.NETHER_STAR);
-    }),
-    SLIME(150, 5.0f, 1, 0, 0, () -> {
+    SLIME(233, 5.0f, 1, 0, 0, () -> {
         return Ingredient.fromItems(SAItems.slimeCrystal.get());
     }),
-    XRAY(1679, 11.0f, 9, 4, 12, () -> {
-        return Ingredient.fromItems(SAItems.xrayIngot.get());
+    WOLF(233, 20.0f, 52.8f, 3, 0, () -> {
+        return Ingredient.fromItems(Items.NETHER_STAR);
     }),
     BH3(886, 11.0f, 15, 3, 10, () -> {
         return Ingredient.fromItems(SAItems.jiejing.get());
     }),
-    SUPER_XRAY(2647, 17.0f, 18, 5, 16, () -> {
-        return Ingredient.fromItems(SAItems.superXrayIngot.get());
+    RUBY(1456, 11f, 4.0f, 3, 10, () -> {
+        return Ingredient.fromItems(SAItems.rubyIngot.get());
     }),
-    SUPER(2105, 13.0f, 12, 4, 14, () -> {
+    EMERALD(1589, 15.0f, 5.0f, 3, 10, () -> {
+             return Ingredient.fromItems(SAItems.emeraldIngot.get());
+    }),
+    XRAY(1879, 19.0f, 9, 4, 12, () -> {
+        return Ingredient.fromItems(SAItems.xrayIngot.get());
+    }),
+    SUPER(2305, 22.0f, 12, 4, 14, () -> {
         return Ingredient.fromItems(SAItems.superIngot.get());
     }),
-    ULTRA(2967, 19.0f, 25, 6, 20, () -> {
+    DRAGON(2765, 25f, 15, 5, 15, () -> {
+        return Ingredient.fromItems(SAItems.dragonCrystal.get());
+    }),
+    SUPER_XRAY(3147, 30.0f, 18, 5, 16, () -> {
+        return Ingredient.fromItems(SAItems.superXrayIngot.get());
+    }),
+    ULTRA(3967, 35.0f, 25, 6, 20, () -> {
         return Ingredient.fromItems(SAItems.ultraIngot.get());
     }),
-    DRAGON(2365, 15f, 15, 5, 15, () -> {
-        return Ingredient.fromItems(SAItems.dragonCrystal.get());
-    }), //数值无穷表示：Double或Float的POSITIVE_INFINITY（正）或NEGATIVE_INFINITY（负）
+    SPACE(5124, 50.0f, 40.0f, 7, 30, () -> {
+        return Ingredient.fromItems(SAItems.spaceIngot.get());
+    }),
+    SUPER_SPACE(1124, 50.0f, 12.0f, 9, 15, () -> {
+        return Ingredient.fromItems(SAItems.spaceCore.get());
+    }),
+    //数值无穷表示：Double或Float的POSITIVE_INFINITY（正）或NEGATIVE_INFINITY（负）
     OP(9999, 99.0f, 10.0f, 99, 0, () -> Ingredient.EMPTY);
 
     private final int maxUses;//耐久
