0a7cbd29ae651f6242727ccf7323c96b52eaee61
yuo <2685245554@qq.com>
2026-01-17 11:39:06 +0800

    fix bug

--- src/main/java/com/yuo/spacearms/SATabs.java
+++ src/main/java/com/yuo/spacearms/SATabs.java
@@ -1,19 +1,16 @@
 package com.yuo.spacearms;
 
-import com.yuo.endless.Endless;
-import com.yuo.endless.Items.EndlessItems;
 import com.yuo.spacearms.Items.SAItems;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.*;
-import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
 //创造模式物品栏 实例化
 public class SATabs {
-	public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Endless.MOD_ID);
-	public static final RegistryObject<CreativeModeTab> SA_TAB0 = TABS.register(Endless.MOD_ID + "_tab0", () -> CreativeModeTab.builder()
+	public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, SpaceArms.MOD_ID);
+	public static final RegistryObject<CreativeModeTab> SA_TAB0 = TABS.register(SpaceArms.MOD_ID + "_tab0", () -> CreativeModeTab.builder()
 			.title(Component.translatable("itemGroup.tab.SpaceArms0"))
 			.icon(() -> SAItems.jade.get().getDefaultInstance())
 			.displayItems((parameters, output) -> {
@@ -23,7 +20,7 @@ public class SATabs {
 					} else output.accept(new ItemStack(entry.get()));
 				}
 			}).build());
-	public static final RegistryObject<CreativeModeTab> SA_TAB1 = TABS.register(Endless.MOD_ID + "_tab1", () -> CreativeModeTab.builder()
+	public static final RegistryObject<CreativeModeTab> SA_TAB1 = TABS.register(SpaceArms.MOD_ID + "_tab1", () -> CreativeModeTab.builder()
 			.title(Component.translatable("itemGroup.tab.SpaceArms1"))
 			.icon(() -> SAItems.opSword.get().getDefaultInstance())
 			.displayItems((parameters, output) -> {
